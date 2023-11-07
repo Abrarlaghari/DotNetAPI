28456c8827d41c7a5e0500a23c9be5520bbd1211
jhussain786 <jamillaghari2000@gmail.com>
2023-11-08 00:16:27 +0500

    Adding Entity

---
+++ BlogPost/Domain/Entities/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Premitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public sealed class BlogPost : Entity
+    {
+        public BlogPost(Guid id, string blogTitle,string author, string content)
+        {
+            BlogTitle=blogTitle;
+            Author=author;
+            Content=content; 
+
+
+        }
+
+        public string BlogTitle { get; private set; }
+        public string Author { get; private set; }
+
+        public string Content { get; private set; }
+    }
+}
