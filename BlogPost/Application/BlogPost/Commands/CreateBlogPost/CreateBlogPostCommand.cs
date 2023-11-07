5eb6e8db9b39ad1b4160929a5418e6d9cb24d9d4
jhussain786 <jamillaghari2000@gmail.com>
2023-11-08 00:42:19 +0500

    Added classes in Application and domain layer

---
+++ BlogPost/Application/BlogPost/Commands/CreateBlogPost/CreateBlogPostCommand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Application.BlogPost.Commands.CreateBlogPost;
+//public sealed record CreateBlogPostCommand(string BlogTitle) : ICommand<Guid>;
