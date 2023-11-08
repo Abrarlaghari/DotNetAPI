b9ee171a894c530f68b4c3af9cdc4b79d46cd656
jhussain786 <jamillaghari2000@gmail.com>
2023-11-08 23:45:25 +0500

    Adding Entity framework in infrastructure layer

--- BlogPost/Infrastructure/ApplicationDbContext.cs
+++ BlogPost/Infrastructure/ApplicationDbContext.cs
@@ -4,10 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Domain.Entities;
 
 namespace Infrastructure
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> blogPosts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+  
+            modelBuilder.Entity<BlogPost>()
+                .HasKey(e => e.Id);
+         
+        }
+
+
+
     }
 }
